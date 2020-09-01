438fbdd3a28ff5d0716a98ecae0e4b966f44a275
Tuyana Dondokova <tuyana.dondokova@gmail.com>
2020-09-02 01:04:43 +0800

    chore(core): setup project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: "always",
+  semi: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "consistent",
+  trailingComma: "all",
+  useTabs: false,
+}
